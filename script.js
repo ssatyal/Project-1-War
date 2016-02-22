8465326993b28f9aa48320b9f2a0b32d76fe0d9b
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-22 13:29:22 -0500

    added suit worth

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 var suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 var valWorth = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+var suitWorth = [0, 1, 2, 3];
 var deck = [];
 var playerHand = [];
 var computerHand = [];
@@ -9,7 +10,7 @@ var computerHand = [];
 function buildDeck() {
   for (var i = 0; i < values.length; i++) {
     for (var j = 0; j < suits.length; j++){
-        deck.push([values[i],suits[j], valWorth[i]]);
+        deck.push([values[i],suits[j], valWorth[i], suitWorth[i]]);
     }
   }
 }
@@ -41,20 +42,45 @@ function shuffleDeck(array) {
     }
   }
 
-//let's evaluate one hand for now
+
 function evalHands () {
   console.log('player: '+playerHand[0]);
   console.log('computer: '+computerHand[0]);
-  if (playerHand[0][2] > computerHand[0][2])
-  {console.log('player won');
+  //if the player wins
+  {if (playerHand[0][2] > computerHand[0][2])
+    {console.log('player won');
+  //computer hand is transferred to player deck
+  playerHand.push(computerHand[0]);
+  //computer hand is removed from computer deck
+  computerHand.splice(0, 1);
+  //player hand should move to the back
+  playerHand.push(playerHand[0]);
+  playerHand.splice(0, 1);
   }else if (playerHand[0][2] < computerHand[0][2]){
   console.log('computer won');
-  }else{console.log('tie!');
-  }
+  //player hand put at end of comp deck
+  computerHand.push(playerHand[0]);
+  //player hand removed from player deck
+  playerHand.splice(0, 1);
+  //computer hand should move to the back
+  computerHand.push(computerHand[0]);
+  computerHand.splice(0, 1);
+}else{console.log('tie! suits determine winner...');
+  //computer hand moved to back
+  computerHand.push(computerHand[0]);
+  computerHand.splice(0, 1);
+  //player hand moved to back
+  playerHand.push(playerHand[0]);
+  playerHand.splice(0, 1);
+}}
+  console.log("Player deck: "+playerHand.length);
+  console.log("Computer deck: "+computerHand.length);
 }
 
 //calling functions here:
 buildDeck();
 shuffleDeck(deck);
 deal();
-evalHands();
+while (playerHand.length > 0 && computerHand.length > 0) {
+  evalHands();
+}
