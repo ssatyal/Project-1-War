5757ab0b5a756fbc53296e519b15cccd2d68a565
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-22 11:14:58 -0500

    removed console log from deck build

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var deck = [];
 function buildDeck() {
   for (var i = 0; i < values.length; i++) {
     for (var j = 0; j < suits.length; j++){
-      console.log(values[i]+' '+suits[j]);
         deck.push(values[i] +" "+ suits[j]);
     }
   }
