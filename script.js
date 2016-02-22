7d3b482e0548b9c38f741581d65fde173106ed19
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-02-22 11:21:53 -0500

    added shuffle function

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ function buildDeck() {
     }
   }
 }
+
+function shuffleDeck(array) {
+  //Fisher Yates shuffle example used below, looked up online
+  var m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+    return array;
+}
+
+//calling functions here:
 buildDeck();
+shuffleDeck(deck);
